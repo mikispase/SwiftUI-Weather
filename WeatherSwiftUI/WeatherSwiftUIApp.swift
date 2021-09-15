8e9d9e66dd0e7c8d73b8fdd813b517141c9d02cb
Dimitar Spasovski <dimitar.spasovski@movic.nl>
2021-09-16 01:43:52 +0200

    Initial Commit

---
+++ WeatherSwiftUI/WeatherSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherSwiftUIApp.swift
+//  WeatherSwiftUI
+//
+//  Created by Dimitar Spasovski on 9/16/21.
+//
+
+import SwiftUI
+
+@main
+struct WeatherSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
